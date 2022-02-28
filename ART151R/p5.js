f4c36b765ccf97a2bd9b100ff823cf13a18d2ae9
CortlandRoberts <59711491+CortlandRoberts@users.noreply.github.com>
2022-02-28 15:48:31 -0800

    Add files via upload
    Continue for the next day.

---
+++ ART151R/p5.js
@@ -0,0 +1,27 @@
+function setup() {
+  createCanvas(500, 500);
+
+
+}
+
+function draw() {
+  background(500);
+
+
+
+  fill(300, 300, 200)
+
+
+
+
+
+  arc(250, 400, 300, 150, 150, PI + QUARTER_PI, CHORD);
+
+  ellipse(230, 300, 100, 300);
+  ellipse(260, 300, 100, 300);
+
+
+  fill(0)
+  ellipse(250, 400, 130, 120);
+
+}
